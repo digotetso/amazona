acf3e68a547004f5c4df598599bf9c98fbe46923
Digotetso Matema <digotetsomatema@Digotetsos-MacBook-Air.local>
2022-06-25 20:52:19 +0200

    Create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Amazona</a>
+      </header>
+
+      <main>
+        Product List
+      </main>
+    </div>
+  );
+}
+
+export default App;
