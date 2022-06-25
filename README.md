3c235d02c882ab2f7635ae28ce45db121ee25b15
Digotetso Matema <digotetsomatema@Digotetsos-MacBook-Air.local>
2022-06-25 22:36:52 +0200

    rendering the product

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 2. Install tools
 3. Create React App
 4. Create git repo
+5. Create products
+    1. Cceate products array
+    2. add product imaages
+    3. render products
+    4. style products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a>Amazona</a>
+        <a href="/home">Amazona</a>
       </header>
 
       <main>
-        Product List
+        <h1 className="heading">Featured products</h1>
+        <div className="products">
+          {data.products.map(product => (
+            <div key={product.slug}>
+              <div className="product">
+                <img src={product.image} alt={product.name} />
+                <div className="product-info">
+                  <a href={`products/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p> <strong>${product.price}</strong></p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
 }
+
 
 export default App;
